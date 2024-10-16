8e12a98f353843b17afe20cc84880c559c1efff6
05-ram <karthik98@gmail.com>
2024-10-16 17:59:28 +0530

    course api added and integrated

--- src/features/course/courseSlice.js
+++ src/features/course/courseSlice.js
@@ -1,11 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { courses } from "../../data";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchCourses = createAsyncThunk('course/get', async () => {
+    const res = await fetch('https://282d1380-70fe-47d4-ae32-0d810977014e.mock.pstmn.io/courses')
+    const data = await res.json();
+    return data?.courses ?? [];
+})
 
 const courseSlice = createSlice(
     {
         name: 'course',
-        initialState: courses,
-        reducers: {}
+        initialState: {
+            data: [],
+            isLoading: false,
+            error: null
+        },
+        reducers: {},
+        extraReducers: (builder) => {
+            builder
+                .addCase(fetchCourses.pending, (state) => {
+                    state.isLoading = true;
+                    state.error = null;
+                })
+                .addCase(fetchCourses.fulfilled, (state, action) => {
+                    state.isLoading = false;
+                    state.data = action.payload;
+                    state.error = null;
+                })
+                .addCase(fetchCourses.rejected, (state, action) => {
+                    state.isLoading = false;
+                    state.error = action.error.message;
+                })
+
+        }
     }
 )
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import img1 from "../assets/images/img1.jpg";
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Spinner } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 // import { courses } from '../data';
 import { useNavigate } from 'react-router-dom';
 import { setSelectedCourse } from '../features/course/selectedSlice';
+import { fetchCourses } from '../features/course/courseSlice';
 
 const Home = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const courses = useSelector(state => state.course)
+
+    const courses = useSelector(state => state.course?.data);
+    const isLoading = useSelector(state => state.course.isLoading);
+    const error = useSelector(state => state.course.error);
+
+    console.log('--------Home------', courses)
+
+    useEffect(() => {
+        dispatch(fetchCourses());
+    }, [])
+
     const handleCourse = (course) => {
         dispatch(setSelectedCourse(course))
         navigate('/course-details')
     }
+
+    if (isLoading) {
+        return (
+            <div className="d-flex justify-content-center wrapper-container align-items-center">
+                <Spinner variant="success" animation='border' />
+            </div>
+        )
+    }
+    if (error) {
+        return (
+            <div className="d-flex justify-content-center">
+                {error}
+            </div>
+        )
+    }
     return (
         <div className='wrapper-container mb-2'>
             <div className='card-deck d-grid gap-4 p-4'>
-                {/* <img src={img1} /> */}
+
                 {
-                    courses.map(course => (
-                        < Card key={course.id}>
-                            <Card.Img variant="top" src={course.url} height={150} />
-                            <Card.Body>
-                                <Card.Title>{course.title}</Card.Title>
-                                <Card.Text>
-                                    {course.description}
-                                </Card.Text>
-                                <Button variant="primary" onClick={() => handleCourse(course)}>Open</Button>
-                            </Card.Body>
-                        </Card>
-                    ))
+                    courses?.length > 0 ? (
+                        courses.map(course => (
+                            < Card key={course.id}>
+                                <Card.Img variant="top" src={course.url} height={150} />
+                                <Card.Body>
+                                    <Card.Title>{course.course_name}</Card.Title>
+                                    <Card.Text>
+                                        {course.description}
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={() => handleCourse(course)}>Open</Button>
+                                </Card.Body>
+                            </Card>
+                        ))
+                    ) :
+
+                        (
+                            <div>No Courses Available...</div>
+                        )
                 }
             </div>
         </div >
